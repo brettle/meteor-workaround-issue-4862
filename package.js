8599614a3d0f32f46143287ef5ef9da729d24875
Dean Brettle <dean@brettle.com>
2015-09-26 08:49:35 -0700

    Publish 0.0.3 with fixes to work with Meteor 1.2.

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   name: 'brettle:workaround-issue-4862',
-  version: '0.0.2',
+  version: '0.0.3',
   summary:
     'Workaround Meteor issue 4862 - user/userId broken in server onLogin',
   git: 'https://github.com/brettle/meteor-workaround-issue-4862.git',
@@ -19,6 +19,7 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
+  api.use('ddp');
   api.use('brettle:accounts-testing-support');
   api.use('brettle:workaround-issue-4862');
   api.addFiles('workaround-issue-4862-tests.js', 'server');
